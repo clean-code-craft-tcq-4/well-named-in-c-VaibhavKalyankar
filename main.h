6a89ba03b96042185cd700a0f0a53fba24924599
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-06-15 20:04:07 +0530

    Create main.h

---
+++ main.h
@@ -0,0 +1,4 @@
+ColorPair GetColorFromPairNumber(int);
+void TestNumberToPair(int,enum MajorColor,enum MinorColor);
+void TestPairToNumber(enum MajorColor,enum MinorColor,int);
+int GetPairNumberFromColor(const ColorPair *);
