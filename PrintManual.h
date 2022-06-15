e02b4476dddc6d6d571b5f5bd003e0a9ba047d6a
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-06-15 17:21:35 +0530

    Update PrintManual.h

--- PrintManual.h
+++ PrintManual.h
@@ -21,3 +21,4 @@ void ColorPairToString(const ColorPair*, char*);
 ColorPair GetColorFromPairNumber(int);
 void TestNumberToPair(int,enum MajorColor,enum MinorColor);
 void TestPairToNumber(enum MajorColor,enum MinorColor,int);
+int GetPairNumberFromColor(const colorPair *);
