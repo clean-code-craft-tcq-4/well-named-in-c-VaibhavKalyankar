60840fb7e77419e749b069e6b82b9daf76ef43fc
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-06-15 09:47:58 +0530

    Update PrintManual.c

--- PrintManual.c
+++ PrintManual.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "PrintManual.h"
+//#include "PrintManual.h"
 void ColorPairToString(const ColorPair*, char*);
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],MinorColorNames[colorPair->minorColor]);
