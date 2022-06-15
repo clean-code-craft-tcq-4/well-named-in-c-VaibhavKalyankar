73405787120d9bc16a911229a5da7db88ee53e35
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-06-15 20:27:36 +0530

    Update main.c

--- main.c
+++ main.c
@@ -14,9 +14,7 @@ int main() {
     }
     return 0;
 }
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],MinorColorNames[colorPair->minorColor]);
-}
+
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
