47a89c209200051d624bdbaf600a4b5adf8cab7c
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-06-15 09:33:52 +0530

    Create PrintManual.c

---
+++ PrintManual.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "PrintManual.h"
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    int zeroBasedPairNumber = pairNumber - 1;
+    colorPair.majorColor = 
+        (enum MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+    colorPair.minorColor =
+        (enum MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+    return colorPair;
+}
