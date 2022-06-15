683e8ff26394359b27ce8066249d1a866b256f9a
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-06-15 20:20:12 +0530

    Delete PrintManual.c

--- PrintManual.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "PrintManual.h"
-
-
-
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
-    sprintf(buffer, "%s %s",MajorColorNames[colorPair->majorColor],MinorColorNames[colorPair->minorColor]);
-}
