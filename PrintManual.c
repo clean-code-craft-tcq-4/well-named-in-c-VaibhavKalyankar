a21dc2d9a6233da944c18710f056e03ad4c1cab4
VaibhavKalyankar <105204227+VaibhavKalyankar@users.noreply.github.com>
2022-06-15 14:17:02 +0530

    Update PrintManual.c

--- PrintManual.c
+++ PrintManual.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "PrintManual.h"
+
 int GetPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
